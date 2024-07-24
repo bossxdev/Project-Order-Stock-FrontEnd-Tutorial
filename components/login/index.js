fcfe81d007781185c8273896690ab9eaa742767d
“Suphanu_Boss” <suphanut_boss@outlook.com>
2024-07-25 01:07:49 +0700

    Create Login component

---
+++ components/login/index.js
@@ -0,0 +1,58 @@
+import React from 'react'
+import {Card, Col, Row, Form, Input, Checkbox, Button} from 'antd'
+
+const Login = (props) => {
+    const onFinish = async (values) => {
+        console.log("values: ", values)
+    }
+
+    return (
+        <Row>
+            <Col span={8}/>
+            <Col span={8}>
+                <Card style={{width: '100%'}}>
+                    <Form
+                        name="loginForm"
+                        layout="vertical"
+                        initialValues={{
+                            remember: true,
+                            username: 'inpangxm@gmail.com',
+                            password: '12345678'
+                        }}
+                        onFinish={onFinish}>
+                        <Form.Item
+                            label="Username"
+                            name="username"
+                            rules={[
+                                {required: true, message: 'Please input your username!'}
+                            ]}>
+                            <Input/>
+                        </Form.Item>
+                        <Form.Item
+                            label="Password"
+                            name="password"
+                            rules={[
+                                {required: true, message: 'Please input your password!'}
+                            ]}>
+                            <Input.Password/>
+                        </Form.Item>
+                        <Form.Item name="remember" valuePropName="checked">
+                            <Checkbox>Remember me</Checkbox>
+                        </Form.Item>
+                        <Form.Item>
+                            <Button
+                                type="primary"
+                                htmlType="submit"
+                                style={{width: '100%'}}>
+                                SIGN IN
+                            </Button>
+                        </Form.Item>
+                    </Form>
+                </Card>
+            </Col>
+            <Col span={8}/>
+        </Row>
+    )
+}
+
+export default Login
