9b5625a7b2b9dfc8d3ef30a312b1284f2ad8f7c2
“Suphanu_Boss” <suphanut_boss@outlook.com>
2024-07-25 01:18:40 +0700

    Create Warehouse page

---
+++ pages/warehouse/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import WarehousePage from "components/warehouse";
+export default function Warehouse() {
+    return (
+        <>
+            <WarehousePage/>
+        </>
+    )
+}
