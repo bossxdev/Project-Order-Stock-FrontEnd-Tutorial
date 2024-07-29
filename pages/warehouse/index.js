d8d0589d04d204ffd927754a244fd26d06273809
“Suphanu_Boss” <suphanut_boss@outlook.com>
2024-07-29 23:58:51 +0700

    Update useEffect for check token cookie

--- pages/warehouse/index.js
+++ pages/warehouse/index.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, {useEffect} from 'react';
+import {useRouter} from 'next/router';
+import Cookies from 'js-cookie';
 import WarehousePage from "components/warehouse";
+
 export default function Warehouse() {
+    const router = useRouter();
+
+    useEffect(() => {
+        const token = Cookies.get('token');
+        if (!token) {
+            router.push('/login'); // Redirect to login page if not authenticated
+        }
+    }, []);
+
     return (
         <>
             <WarehousePage/>
         </>
-    )
+    );
 }
