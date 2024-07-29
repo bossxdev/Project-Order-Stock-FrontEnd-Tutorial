4acdbc15899e447ed5c49c62de52c72dbdffe10f
“Suphanu_Boss” <suphanut_boss@outlook.com>
2024-07-29 23:57:36 +0700

    Refactor Axios instance

--- src/api/Http.js
+++ src/api/Http.js
@@ -1,6 +1,4 @@
 import axios from 'axios'
-import Cookies from 'js-cookie'
-import { HTTP_STATUS_CODE } from 'utils/constants'
 
 const Http = axios.create({
   baseURL: 'http://localhost:5000',
@@ -14,36 +12,5 @@ const Http = axios.create({
       'ManageContent-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With'
   }
 })
-
-Http.interceptors.request.use(
-  function (config) {
-    const token = Cookies.get('token')
-
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`
-    }
-    return config
-  },
-  function (error) {
-    return Promise.reject(error)
-  }
-)
-
-Http.interceptors.response.use(
-  function (response) {
-    return response
-  },
-  function (error) {
-    if (
-      error != null &&
-      error.response != null &&
-      error.response.status === HTTP_STATUS_CODE.UNAUTHORIZED
-    ) {
-      Cookies.remove('token')
-      window.location = '/warehouse'
-    }
-    return Promise.reject(error)
-  }
-)
 
 export default Http
