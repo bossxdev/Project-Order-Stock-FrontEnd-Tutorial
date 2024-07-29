dea9d39f8f4dddca6726d4cd7728508c941ea15d
“Suphanu_Boss” <suphanut_boss@outlook.com>
2024-07-29 23:58:02 +0700

    Refactor Login page

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,10 @@
 import React from "react";
-import Login from "components/login";
+import LoginPage from "./login";
 
 export default function Home() {
     return (
         <>
-            <Login />
+            <LoginPage />
         </>
     );
 }
