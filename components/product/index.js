e64e747361a0b210a42f7110afa15d0ee25f7f48
“Suphanu_Boss” <suphanut_boss@outlook.com>
2024-07-31 01:35:41 +0700

    Clean code Product modal component

--- components/product/index.js
+++ components/product/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import {Form, Input, Modal} from "antd";
 
 export default function CreateProductModal({ visible, onCreate, onCancel }) {
