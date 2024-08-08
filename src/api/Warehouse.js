ee33225c50ae88826a2e62f6395d8fcf94ab568e
“Suphanu_Boss” <suphanut_boss@outlook.com>
2024-08-09 02:36:01 +0700

    Update warehouseById api

--- src/api/Warehouse.js
+++ src/api/Warehouse.js
@@ -10,3 +10,13 @@ export const warehouse = async () => {
         return err;
     }
 }
+
+export const warehouseById = async (id) => {
+    try {
+        const response = await API.get(EndPoints.WAREHOUSE + '/warehouseById/' + id)
+        return response.data;
+    } catch (err) {
+        console.log(err);
+        return err;
+    }
+}
