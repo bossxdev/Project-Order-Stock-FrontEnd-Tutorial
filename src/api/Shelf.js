164b655ef894a23102d63c65123aae8cc3c5baac
bossxdev <suphanut_boss@outlook.com>
2024-08-12 14:56:07 +0700

    Create Shelf http

---
+++ src/api/Shelf.js
@@ -0,0 +1,22 @@
+import API from 'api/Http'
+import * as EndPoints from 'api/EndPoints'
+
+export const shelf = async () => {
+    try {
+        const response = await API.get(EndPoints.SHELF)
+        return response;
+    } catch (err) {
+        console.log(err);
+        return err;
+    }
+}
+
+export const shelfById = async (id) => {
+    try {
+        const response = await API.get(EndPoints.SHELF + '/shelfById/' + id)
+        return response.data;
+    } catch (err) {
+        console.log(err);
+        return err;
+    }
+}
