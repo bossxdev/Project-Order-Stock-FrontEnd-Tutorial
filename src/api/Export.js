a20821fdc16f20ab880dbe9cab9217fc498034d4
“Suphanu_Boss” <suphanut_boss@outlook.com>
2024-08-14 18:40:26 +0700

    Refactor api call export

--- src/api/Export.js
+++ src/api/Export.js
@@ -2,19 +2,9 @@ import API from 'api/Http'
 import * as EndPoints from 'api/EndPoints'
 import { HTTP_STATUS_CODE } from 'utils/constants'
 
-export const exports = async (data) => {
+export const exports = async () => {
     try {
         const response = await API.get(EndPoints.EXPORT)
-        return response;
-    } catch (err) {
-        console.log(err);
-        return err;
-    }
-}
-
-export const exportsById = async (id) => {
-    try {
-        const response = await API.get(EndPoints.EXPORT + '/exportByWarehouseId/' + id)
         return response.data;
     } catch (err) {
         console.log(err);
